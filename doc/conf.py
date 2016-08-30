119702e2215f8b87b29766ac67e3607c1b60fc2c
Jan Varada <jan.varada@nih.gov>
2016-08-30 13:41:33 -0400

    Ran 2to3 script.

--- doc/conf.py
+++ doc/conf.py
@@ -61,8 +61,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'dcmstack'
-copyright = u'2012, Brendan Moloney'
+project = 'dcmstack'
+copyright = '2012, Brendan Moloney'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -199,8 +199,8 @@ htmlhelp_basename = 'dcmstackdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'dcmstack.tex', u'dcmstack Documentation',
-   u'Brendan Moloney', 'manual'),
+  ('index', 'dcmstack.tex', 'dcmstack Documentation',
+   'Brendan Moloney', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -232,6 +232,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'dcmstack', u'dcmstack Documentation',
-     [u'Brendan Moloney'], 1)
+    ('index', 'dcmstack', 'dcmstack Documentation',
+     ['Brendan Moloney'], 1)
 ]
